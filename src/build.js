20aedd905b183b8aeef9fbbff4f0a93c5dc2b90c
Jonathan Hooper <jonathan.hooper@gsa.gov>
2016-10-12 15:00:25 -0500

    Add build class
    This commit adds a class named "Build" which represents a site build.
    The Build classes concerns include:
    
    - Reading container overrides from sqs messages
    - Assigning builds a unique identifier

---
+++ src/build.js
@@ -0,0 +1,40 @@
+const crypto = require("crypto")
+const url = require("url")
+
+class Build {
+  constructor(sqsMessage) {
+    this.sqsMessage = sqsMessage
+    this.buildID = this._generateBuildID()
+    this.containerEnvironment = this._resolveContainerEnvironment(
+      this.buildID,
+      this.sqsMessage
+    )
+  }
+
+  _buildCallbackURL(buildID) {
+    return process.env.BUILD_COMPLETE_CALLBACK.replace("{buildID}", buildID)
+  }
+
+  _generateBuildID() {
+    return crypto.randomBytes(48).toString('base64').
+                  replace(/\+/g, '-').
+                  replace(/\//g, '_').
+                  replace(/\=/g, '')
+  }
+
+  _resolveContainerEnvironment(buildID, sqsMessage) {
+    const environmentOverrides = JSON.parse(sqsMessage.Body).environment
+
+    let environment = environmentOverrides.reduce((environment, environmentOverride) => {
+      return Object.assign(environment, {
+        [environmentOverride["name"]]: environmentOverride["value"]
+      })
+    }, {})
+
+    environment["FEDERALIST_BUILDER_CALLBACK"] = this._buildCallbackURL(buildID)
+
+    return environment
+  }
+}
+
+module.exports = Build
