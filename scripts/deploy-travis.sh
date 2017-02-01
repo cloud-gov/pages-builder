6e6622bf9e0a3ab2e6c4194779f354aa15f85259
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-02-01 13:39:10 -0600

    Bump CF CLI version to 6.23.1

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -16,12 +16,12 @@ else
   exit
 fi
 
-wget https://s3.amazonaws.com/go-cli/releases/v6.12.4/cf-cli_amd64.deb -qO temp.deb && sudo dpkg -i temp.deb
+wget https://s3.amazonaws.com/go-cli/releases/v6.23.1/cf-cli_amd64.deb -qO temp.deb && sudo dpkg -i temp.deb
 
 rm temp.deb
 
 cf api $CF_API
-cf login --u $CF_USERNAME --p $CF_PASSWORD --o $CF_ORGANIZATION --s $CF_SPACE
+cf login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
 
 cf zero-downtime-push $CF_APP -f $CF_MANIFEST
 
