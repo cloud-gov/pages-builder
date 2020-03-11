5df5b39547e7a6a0d02117b84b046327ae0b9f09
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2020-03-11 15:50:54 -0400

    remove new relic

--- app.js
+++ app.js
@@ -8,16 +8,7 @@ const SQSClient = require('./src/sqs-client');
 
 const appEnv = cfenv.getAppEnv();
 
-const { APP_ENV, NEW_RELIC_APP_NAME } = process.env;
-
-// If settings present, start New Relic
-if (NEW_RELIC_APP_NAME) {
-  const creds = appEnv.getServiceCreds('federalist-builder-env');
-  if (creds.NEW_RELIC_LICENSE_KEY) {
-    logger.info(`Activating New Relic: ${NEW_RELIC_APP_NAME}`);
-    require('newrelic'); // eslint-disable-line global-require
-  }
-}
+const { APP_ENV } = process.env;
 
 const queueURL = appEnv.getServiceCreds(`federalist-${APP_ENV}-sqs-creds`).sqs_url;
 
