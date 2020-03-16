0661cfe3eac28a00d5bee16880d10dd16e8d9b69
David Corwin <david.corwin@gsa.gov>
2020-03-16 11:35:07 -0700

    Remove refs to New Relic in appEnv

--- env.js
+++ env.js
@@ -2,7 +2,6 @@ const cfenv = require('cfenv');
 
 const {
   CLOUD_FOUNDRY_OAUTH_TOKEN_URL,
-  NEW_RELIC_APP_NAME,
   NODE_ENV,
 } = process.env;
 
@@ -12,7 +11,6 @@ const appEnv = cfenv.getAppEnv({ vcapFile });
 const { cf_api: cfApiHost, space_id: spaceId, space_name: spaceName } = appEnv.app;
 
 const cfCreds = appEnv.getServiceCreds('federalist-deploy-user');
-const apmCreds = appEnv.getServiceCreds('federalist-builder-env');
 const sqsCreds = appEnv.getServiceCreds(`federalist-${spaceName}-sqs-creds`);
 
 // Some helpful attributes
@@ -22,12 +20,6 @@ appEnv.cloudFoundryCreds = {
   password: cfCreds.DEPLOY_USER_PASSWORD || cfCreds.password,
 };
 appEnv.cloudFoundryApiHost = cfApiHost;
-
-appEnv.isAPMConfigured = NEW_RELIC_APP_NAME && apmCreds && apmCreds.NEW_RELIC_LICENSE_KEY;
-appEnv.newRelicConfig = appEnv.isAPMConfigured && {
-  app_name: [NEW_RELIC_APP_NAME],
-  license_key: apmCreds.NEW_RELIC_LICENSE_KEY,
-};
 
 appEnv.spaceGUID = spaceId;
 
