7c9c250dd757d05c358fca4027f20c5dca69f2c2
David Corwin <david.corwin@gsa.gov>
2020-09-11 13:09:39 -0700

    Remove build log request

--- src/build-status-reporter.js
+++ src/build-status-reporter.js
@@ -5,16 +5,6 @@ function _request(url, json) {
   return axios.post(url, json);
 }
 
-function _sendBuildLogRequest(build, buildLogOutput) {
-  logger.verbose(`Sending build log request for ${build.buildID}: ${buildLogOutput}`);
-
-  const url = build.containerEnvironment.LOG_CALLBACK;
-  return _request(url, {
-    output: Buffer.from(buildLogOutput).toString('base64'),
-    source: 'Build scheduler',
-  });
-}
-
 function _sendBuildStatusRequest(build, buildStatus, buildErrorMsg) {
   let logMsg = `Sending ${buildStatus} status request for ${build.buildID}`;
   if (buildErrorMsg.length) {
@@ -32,16 +22,9 @@ function _sendBuildStatusRequest(build, buildStatus, buildErrorMsg) {
 function reportBuildStatus(build, buildStatus) {
   const buildErrorMsg = buildStatus === 'error' ? 'The build timed out' : '';
 
-  let buildLogOutput = `Build status updated to ${buildStatus}`;
-  if (buildErrorMsg.length) {
-    buildLogOutput = `${buildLogOutput}: ${buildErrorMsg}`;
-  }
-
-  return Promise.all([
-    _sendBuildLogRequest(build, buildLogOutput),
-    _sendBuildStatusRequest(build, buildStatus, buildErrorMsg),
-  ]).catch((err) => {
-    logger.error('Error reporting build status:', err);
-  });
+  return _sendBuildStatusRequest(build, buildStatus, buildErrorMsg)
+    .catch((err) => {
+      logger.error('Error reporting build status:', err);
+    });
 }
 module.exports = { reportBuildStatus };
