0333de8b81f52d7d526added57179101e1cba222
David Corwin <david.corwin@gsa.gov>
2020-09-11 13:34:15 -0700

    Remove all traces of Application pool and log callback

--- test/build-status-reporter-test.js
+++ test/build-status-reporter-test.js
@@ -1,9 +1,8 @@
 const { expect } = require('chai');
 const nock = require('nock');
 const url = require('url');
-const mockBuildLogCallback = require('./nocks/build-log-callback-nock');
-const mockBuildStatusCallback = require('./nocks/build-status-callback-nock');
 
+const mockBuildStatusCallback = require('./nocks/build-status-callback-nock');
 const BuildStatusReporter = require('../src/build-status-reporter');
 
 describe('BuildStatusReporter', () => {
@@ -12,45 +11,35 @@ describe('BuildStatusReporter', () => {
   });
 
   describe('reportBuildStatus @buildStatus=error', () => {
-    it("should send a request to the build's status and log callback", async () => {
+    it("should send a request to the build's status callback", async () => {
       const buildStatus = 'error';
-      const logOutput = `Build status updated to ${buildStatus}: The build timed out`;
-      const logURL = url.parse('https://www.example.gov/log');
       const statusURL = url.parse('https://www.example.gov/status');
-      const logCallbackNock = mockBuildLogCallback(logURL, logOutput);
       const statusCallbackNock = mockBuildStatusCallback(statusURL, buildStatus);
 
       const build = {
         containerEnvironment: {
-          LOG_CALLBACK: logURL.href,
           STATUS_CALLBACK: statusURL.href,
         },
       };
 
       await BuildStatusReporter.reportBuildStatus(build, 'error');
-      expect(logCallbackNock.isDone()).to.be.true;
       expect(statusCallbackNock.isDone()).to.be.true;
     });
   });
 
   describe('reportBuildStatus @buildStatus <> error', () => {
-    it("should send a request to the build's status and log callback", async () => {
+    it("should send a request to the build's status callback", async () => {
       const buildStatus = 'testStatus';
-      const logOutput = `Build status updated to ${buildStatus}`;
-      const logURL = url.parse('https://www.example.gov/log');
       const statusURL = url.parse('https://www.example.gov/status');
-      const logCallbackNock = mockBuildLogCallback(logURL, logOutput);
       const statusCallbackNock = mockBuildStatusCallback(statusURL, buildStatus);
 
       const build = {
         containerEnvironment: {
-          LOG_CALLBACK: logURL.href,
           STATUS_CALLBACK: statusURL.href,
         },
       };
 
       await BuildStatusReporter.reportBuildStatus(build, buildStatus);
-      expect(logCallbackNock.isDone()).to.be.true;
       expect(statusCallbackNock.isDone()).to.be.true;
     });
   });
