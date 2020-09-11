6e48ea26ed944ffbbf93afac03bd1afeec99816e
David Corwin <david.corwin@gsa.gov>
2020-09-11 14:04:17 -0700

    Remove old env vars from test env

--- test/env.js
+++ test/env.js
@@ -1,5 +1,3 @@
-process.env.BUILD_CONTAINER_BASE_NAME = 'test-builder';
 process.env.CLOUD_FOUNDRY_OAUTH_TOKEN_URL = 'https://login.example.com/oauth/token';
 process.env.LOG_LEVEL = 'crit';
-process.env.NUM_BUILD_CONTAINERS = '2';
 process.env.PORT = 3000;
