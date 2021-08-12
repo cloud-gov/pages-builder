68bb97f489fb10cce126139286cd460ffec394c8
David Corwin <david.corwin@gsa.gov>
2021-08-12 09:08:00 -0700

    Hack service name

--- env.js
+++ env.js
@@ -19,8 +19,11 @@ const appEnv = cfenv.getAppEnv({ vcapFile });
 const { cf_api: cfApiHost, space_name: spaceName } = appEnv.app;
 
 const cfCreds = appEnv.getServiceCreds('federalist-deploy-user');
-const sqsCreds = appEnv.getServiceCreds(`federalist-${spaceName}-sqs-creds`);
-const redisCreds = appEnv.getServiceCreds(`federalist-${spaceName}-redis`);
+
+// TODO - revisit our service naming conventions!!!!
+const servicePrefix = spaceName === 'pages-staging' ? spaceName : `federalist-${spaceName}`;
+const sqsCreds = appEnv.getServiceCreds(`${servicePrefix}-sqs-creds`);
+const redisCreds = appEnv.getServiceCreds(`${servicePrefix}-redis`);
 
 // Some helpful attributes
 appEnv.cloudFoundryOAuthTokenUrl = CLOUD_FOUNDRY_OAUTH_TOKEN_URL;
